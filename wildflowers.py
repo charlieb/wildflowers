dcefcc0d665e4a55a7415bab8e0ba70405e3041d
Charlie Burrows <charlie.burrows@gmail.com>
2016-09-01 23:33:45 -0400

    Initial commit

---
+++ wildflowers.py
@@ -0,0 +1,42 @@
+import flowers as flo
+import dla
+from math import pi
+import svgwrite as svg
+import time
+
+def main():
+    particle_radius = 1.
+    particle_scaler = 1.
+    nparticles = 3000
+    particles = dla.generate_particles(nparticles, particle_radius, particle_scaler, start_angle_range=pi/2)
+    leaves = dla.find_leaves(particles)
+
+    npetals = 7
+    petal_taper=1.5
+    petal_fatness=2.
+    flowers = []
+    for i in leaves:
+        flowers += flo.flower(particles[i][0], particles[i][1], npetals,
+                particles[i][2]*1.5, # petal length = particle radius
+                particles[i][2]*0.5, # petal width
+                petal_taper,
+                petal_fatness)
+
+    line_width=0.1
+    dwg = svg.Drawing('test.svg')
+    dla.draw(particles, dwg, 
+            circles=True, links=True, 
+            circle_color='brown', link_color='green',
+            line_width=line_width,
+            prune=leaves)
+    flo.draw(flowers, dwg, color='blue', line_width=line_width)
+    dwg.save()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    main()
+    t1 = time.time()
+
+    print("Elapsed:", t1-t0)
+
