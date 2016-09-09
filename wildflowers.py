a23aa444b598f84ee879005ebffd28bbec97e063
Charlie Burrows <charlie.burrows@gmail.com>
2016-09-09 00:00:17 -0400

    Added stipple with flowers

--- wildflowers.py
+++ wildflowers.py
@@ -1,10 +1,11 @@
 import flowers as flo
 import dla
+import stipple as sti
 from math import pi
 import svgwrite as svg
 import time
 
-def main():
+def plant():
     particle_radius = 1.
     particle_scaler = 0.9995
     nparticles = 3000
@@ -32,6 +33,32 @@ def main():
     flo.draw(flowers, dwg, color='blue', line_width=line_width)
     dwg.save()
 
+def stipple():
+    points = sti.stipple('../stipple/StippleGen2/data/grace.jpg', 4000, resize=400)
+
+    mindot=0.75
+    maxdot=4.
+    npetals = 7
+    petal_taper=1.5
+    petal_fatness=2.
+    flowers = []
+    for p in points:
+        r = maxdot - (p[2] / 255.) * (maxdot - mindot)
+        flowers += flo.flower(p[0], p[1], npetals,
+                                r, # petal length = particle radius
+                                r*0.5, # petal width
+                                petal_taper,
+                                petal_fatness)
+
+    line_width=0.1
+    dwg = svg.Drawing('test.svg')
+    flo.draw(flowers, dwg, color='blue', line_width=line_width)
+    dwg.save()
+
+
+def main():
+    stipple()
+    #plant()
 
 if __name__ == '__main__':
     t0 = time.time()
